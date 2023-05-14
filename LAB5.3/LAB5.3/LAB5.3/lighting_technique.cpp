f070dc7559777795de0d97d521fdfcf29d465ce9
FullThrottle1 <midnightanimalgithubU1997@gmail.com>
2023-05-15 00:26:54 +0500

    fixed bugs

--- LAB5.3/LAB5.3/LAB5.3/lighting_technique.cpp
+++ LAB5.3/LAB5.3/LAB5.3/lighting_technique.cpp
@@ -50,7 +50,7 @@ struct BaseLight                                                                
                                                                                     \n\
 struct DirectionalLight                                                             \n\
 {                                                                                   \n\
-    struct BaseLight Base;                                                          \n\
+    BaseLight Base;                                                          \n\
     vec3 Direction;                                                                 \n\
 };                                                                                  \n\
                                                                                     \n\
@@ -63,14 +63,14 @@ struct Attenuation                                                              
                                                                                     \n\
 struct PointLight                                                                           \n\
 {                                                                                           \n\
-    struct BaseLight Base;                                                                  \n\
+    BaseLight Base;                                                                  \n\
     vec3 Position;                                                                          \n\
     Attenuation Atten;                                                                      \n\
 };                                                                                          \n\
                                                                                             \n\
 struct SpotLight                                                                            \n\
 {                                                                                           \n\
-    struct PointLight Base;                                                                 \n\
+    PointLight Base;                                                                 \n\
     vec3 Direction;                                                                         \n\
     float Cutoff;                                                                           \n\
 };                                                                                          \n\
